c89ee75e8261b11748a1c294a9337d6133ebc704
Robert Pauls <rpauls2002@gmail.com>
2026-06-17 14:07:59 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-  }
+  },
+  site: 'https://rpauls02.github.io/portfolio/',
+  base: '/portfolio'
 });
